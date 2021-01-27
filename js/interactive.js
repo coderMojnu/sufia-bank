c65de8594d20a422fd5f497cc32d8f9760ad5dc5
Md. Mojnu Miah <mojnu.cse.pust@gmail.com>
2021-01-27 16:49:46 +0600

    done sufia bank

--- js/interactive.js
+++ js/interactive.js
@@ -1,7 +1,7 @@
 //signin button handler
 
 const signInBtn = document.getElementById('signin');
-signInBtn.addEventListener('click', function(){
+signInBtn.addEventListener('click', function () {
     const logInArea = document.getElementById('login-area');
     logInArea.style.display = 'none';
     const transactionArea = document.getElementById('transaction-area');
@@ -11,26 +11,33 @@ signInBtn.addEventListener('click', function(){
 //deposit button handler
 
 const depositBtn = document.getElementById('deposit-btn');
-depositBtn.addEventListener('click', function(){
-    const depositAmount = document.getElementById('deposit-amount').value;
-    const depositNumber = parseFloat(depositAmount);
-    const currentDeposit = document.getElementById('current-deposit').innerText;
-    const currentNumber = parseFloat(currentDeposit);
-    const totalDeposit = depositNumber + currentNumber;
-    document.getElementById('current-deposit').innerText = totalDeposit;
-
-    const currentBalance = document.getElementById('current-balance').innerText;
-    const currentBalanceNumber = parseFloat(currentBalance);
-    const totalBalance = currentBalanceNumber + depositNumber;
-    document.getElementById('current-balance').innerText = totalBalance; 
-
+depositBtn.addEventListener('click', function () {
+    
+    const depositNumber = getInputNumber('deposit-amount');
     document.getElementById('deposit-amount').value = '';
 
+    updateSpanValue("current-deposit", depositNumber);
+    updateSpanValue("current-balance", depositNumber);
 })
 
 //withdraw button handler
 
 const withdrawBtn = document.getElementById('withdraw-btn');
-withdrawBtn.addEventListener('click', function(){
-    alert('withdraw clicked')
+withdrawBtn.addEventListener('click', function () {
+    const withdrawAmount = getInputNumber('withdraw-amount');
+    updateSpanValue("withdraw-balance", withdrawAmount);
+    updateSpanValue("current-balance", -1*withdrawAmount);
+    document.getElementById('withdraw-amount').value = '';
 })
+
+function getInputNumber(id) {
+    const Amount = document.getElementById(id).value;
+    const amountNumber = parseFloat(Amount);
+    return amountNumber;
+}
+function updateSpanValue(id, depositNumber) {
+    const current = document.getElementById(id).innerText;
+    const currentBalance = parseFloat(current);
+    const totalBalance = depositNumber + currentBalance;
+    document.getElementById(id).innerText = totalBalance;
+}
